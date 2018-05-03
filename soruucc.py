fb4e40d6ec79c33e151ec154294ad5d885283c96
Nevruze Karakus <nevruzekarakus@gmail.com>
2018-05-03 15:12:06 +0300

    Update soruucc.py

--- soruucc.py
+++ soruucc.py
@@ -1,12 +1,23 @@
-from soruuc import ybsbir
-from soruuc import ybsiki
-from soruuc import ybsuc
+import soruuc
 
-ybs_bir=ybsbir()
+begenibir=365000
+yorumbir=65000
+paylasimbir=870
+icerikbir=500
+takipcibir=1125000
+ybs_bir=soruuc.etkilesimOrani(begenibir,yorumbir,paylasimbir,icerikbir,takipcibir)
 print(ybs_bir)
-ybs_iki=ybsiki()
+begenii=450000
+yorumi=25000
+paylasimi=380
+iceriki=100
+takipcii=1450000
+ybs_iki=soruuc.etkilesimOrani(begenii,yorumi,paylasimi,iceriki,takipcii)
 print(ybs_iki)
-ybs_uc=ybsuc()
+begeniu=582000
+yorumu=52000
+paylasimu=1200
+iceriku=650
+takipciu=2000000
+ybs_uc=soruuc.etkilesimOrani(begeniu,yorumu,paylasimu,iceriku,takipciu)
 print(ybs_uc)
-
-
