16a9d591666041c3488e391d65c30240ff67ceec
Nevruze Karakus <nevruzekarakus@gmail.com>
2018-05-03 13:41:14 +0300

    Update module2.py

--- module2.py
+++ module2.py
@@ -3,10 +3,9 @@ from module import adambasiCiro
 
 gelir=int(input("Gelirleri giriniz:"))
 gider=int(input("Giderleri giriniz:"))
+isletme_kari=isletmeKari(gelir,gider)
+print(isletme_kari)
 toplamCiro=int(input("Toplam ciroyu giriniz:"))
 toplamCalisanSayisi=int(input("Adambaşı ciroyu giriniz:"))
-
-isletme_kari=isletmeKari(gelir,gider)
 adambasi_ciro=adambasiCiro(toplamCiro,toplamCalisanSayisi)
-print(isletme_kari)
 print(adambasi_ciro)
